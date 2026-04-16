0db5b5521b184a69d2c23fc71c651c1eb7d0827e
devjosecagnini <jose.cagnini@proton.me>
2026-04-16 11:42:21 -0300

    test: add parse test

--- bindings/node/binding_test.js
+++ bindings/node/binding_test.js
@@ -1,11 +1,24 @@
-const assert = require("node:assert");
-const { test } = require("node:test");
+const assert = require('node:assert');
+const { test } = require('node:test');
 
-const Parser = require("tree-sitter");
+const Parser = require('tree-sitter');
+const PowerscriptLanguage = require('./index');
 
-const PowerscriptLanguage = require("./index")
+const fs = require('node:fs');
+const path = require('node:path');
 
-test("can load grammar", () => {
+test('can load grammar', () => {
   const parser = new Parser();
   assert.doesNotThrow(() => parser.setLanguage(PowerscriptLanguage));
+});
+
+test('can parse ../../examples/playground.srf', () => {
+  const parser = new Parser();
+  assert.doesNotThrow(() => parser.setLanguage(PowerscriptLanguage));
+
+  const filePath = path.join(__dirname, '../../examples/playground.srf');
+  const fileText = fs.readFileSync(filePath, 'utf8');
+  const tree = parser.parse(fileText, null);
+
+  assert.equal(tree.rootNode.type, 'source_file');
 });
