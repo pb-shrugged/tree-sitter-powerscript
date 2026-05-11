ed55998b506d62b809bf0a98b1a739794bde28a0
devjosecagnini <jose.cagnini@proton.me>
2026-05-11 10:46:01 -0300

    chore: bump to version 0.0.10

--- src/parser.c
+++ src/parser.c
@@ -210852,7 +210852,7 @@ TS_PUBLIC const TSLanguage *tree_sitter_powerscript(void) {
     .metadata = {
       .major_version = 0,
       .minor_version = 0,
-      .patch_version = 9,
+      .patch_version = 10,
     },
   };
   return &language;
